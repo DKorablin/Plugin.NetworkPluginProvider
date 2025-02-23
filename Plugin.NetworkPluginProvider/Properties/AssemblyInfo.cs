78425c88145ea9a66b4bc740686139c077b643c8
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 15:53:06 +0300

    Unify plugins build

--- Plugin.NetworkPluginProvider/Properties/AssemblyInfo.cs
+++ Plugin.NetworkPluginProvider/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: Guid("03d92867-75d3-47ce-baca-cd88c5e0d6b3")]
-[assembly: ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
 
 #if NETSTANDARD || NETCOREAPP
@@ -11,13 +10,6 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyTitle("Plugin.NetworkPluginProvider")]
 [assembly: AssemblyProduct("Provides access to plugins througth the network")]
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2024")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2025")]
 
 #endif
